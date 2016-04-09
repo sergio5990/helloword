91ea7d72cc2794c57a94dbd2d0846e8781e93b0e
Belotserkovsky <bkostby@gmail.com>
2016-04-09 11:38:59 +0300

    add book class

---
+++ src/main/java/by/academy/it/Book.java
@@ -0,0 +1,21 @@
+package by.academy.it;
+
+/**
+ * Created by Kostya on 09.04.2016.
+ */
+
+/**
+ * Class Book. B entity
+ */
+public class Book {
+
+    String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
